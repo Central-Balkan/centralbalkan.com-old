59ad26dfc76607029613569313ad4d689166ba2e
Ivo Donchev <i.donchev@hacksoft.io>
2019-09-18 19:03:37 +0300

    Remove f string

--- central_balkan/central_balkan/old/views.py
+++ central_balkan/central_balkan/old/views.py
@@ -5,4 +5,6 @@ class OldTemplateView(TemplateView):
     def get_template_names(self):
         template_name = self.kwargs.get('template_name')
 
-        return f'old_site/{template_name}'
+        return 'old_site/{template_name}'.format(
+            template_name=template_name
+        )
